bc0b892872c5b3c6559860c14c99da6b35cec302
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-11 13:16:37 -0500

    implementacion de DTOs y mapper

--- vocacion-service/src/main/java/com/vocaciON/vocacion_service/dto/AuthResponseDTO.java
+++ vocacion-service/src/main/java/com/vocaciON/vocacion_service/dto/AuthResponseDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 public class AuthResponseDTO {
-    private String token; // el token jwt
+    private String token; // el token jw t
     private String nombre; // el primer nombre de usuario
     private String apellido; // el apellido del ususario
     private String role; // el rol del usuario
