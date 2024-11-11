2a03e35bb4931e50f118796586255371ccf2b16e
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-11 13:18:01 -0500

    implementacion de controller

--- vocacion-service/src/main/java/com/vocaciON/vocacion_service/dto/AuthResponseDTO.java
+++ vocacion-service/src/main/java/com/vocaciON/vocacion_service/dto/AuthResponseDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class AuthResponseDTO {
     private String token; // el token jw t
     private String nombre; // el primer nombre de usuario
-    private String apellido; // el apel lido del ususario
+    private String apellido; // el apellido del ususario
     private String role; // el rol del usuario
 
 }
