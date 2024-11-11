5630007e2a8aa6fd31ab1b5f1e913f2221fc81ff
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-11 15:34:56 -0500

    Implementar el usuarioPrincipal

--- vocacion-service/src/main/java/com/vocaciON/vocacion_service/security/UsuarioPrincipal.java
+++ vocacion-service/src/main/java/com/vocaciON/vocacion_service/security/UsuarioPrincipal.java
@@ -24,6 +24,7 @@ public class UsuarioPrincipal implements UserDetails {
 
     private Usuario usuario;
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
