3cbb531ce1bb775fd218d9002e6437ef226c118c
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-13 16:48:22 -0500

    Integrar el modulo de seguridad

--- vocacion-service/src/main/java/com/vocaciON/vocacion_service/config/WebSecurityConfig.java
+++ vocacion-service/src/main/java/com/vocaciON/vocacion_service/config/WebSecurityConfig.java
@@ -1,17 +1,39 @@
 package com.vocaciON.vocacion_service.config;
 
+import com.vocaciON.vocacion_service.security.JWTAuthenticationEntryPoint;
+import com.vocaciON.vocacion_service.security.JWTConfigurer;
+import com.vocaciON.vocacion_service.security.JWTFilter;
+import com.vocaciON.vocacion_service.security.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.security.Security;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JWTFilter jwtRequestFilter;
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final UserDetailsService userDetailsService;
+
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -19,11 +41,32 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher("/auth/login")).permitAll()
+                        .requestMatchers(antMatcher("/auth/register/perfil")).permitAll()
+                        .requestMatchers(antMatcher("/perfil/contenido")).permitAll()// url que tiene que ver cualquiercosa
+
+                        .requestMatchers( "api/v1/swagger-ui/**","v3/api-docs/**","/swagger-ui.html","/swagger-iu/**","/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(AbstractHttpConfigurer::disable)
+                .exceptionHandling(e -> e.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(h -> h.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .with(new JWTConfigurer(tokenProvider), Customizer.withDefaults());
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ vocacion-service/src/main/java/com/vocaciON/vocacion_service/security/TokenProvider.java
@@ -0,0 +1,92 @@
+package com.vocaciON.vocacion_service.security;
+
+import com.vocaciON.vocacion_service.model.entity.Usuario;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.management.relation.RoleInfoNotFoundException;
+import javax.management.relation.RoleNotFoundException;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+
+public class TokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.validity-in-seconds}")
+    private Long jwtValidityInSeconds;
+
+    private Key key;
+    private JwtParser jwtParser;
+
+    @PostConstruct
+    public void init() {
+
+        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+
+        jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+    }
+
+    public String createAccessToken(Authentication authentication) {
+        String email = authentication.getName();
+
+        String role = authentication
+                .getAuthorities()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No se encontró ningún rol para el usuario"))
+                .getAuthority();
+
+        return Jwts
+                .builder()
+                .setSubject(email)
+                .claim("role", role)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtValidityInSeconds*1000))
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+
+        String role = claims.get("role").toString();
+
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        }catch (JwtException e) {
+            return false;
+        }
+
+    }
+    
+
+}
